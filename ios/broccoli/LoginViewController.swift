27d398a4f83f25fe6d27bcb5e6021a7e1e7161bf
Nuno Alexandre <nunombalexandre@gmail.com>
2017-05-02 22:59:32 +0200

    Customising the Locker

--- ios/broccoli/LoginViewController.swift
+++ ios/broccoli/LoginViewController.swift
@@ -17,80 +17,22 @@ class LoginViewController : UIViewController {
         super.viewDidLoad()
         Lock
             .classic()
+            .withStyle {
+                $0.title = "Broccoli"
+                $0.headerBlur = .extraLight
+                $0.logo = LazyImage(name: "broccoli")
+                $0.primaryColor = UIColor ( red: 0.3784, green: 0.7333, blue: 0.6784, alpha: 1.0 )
+            }
             .onAuth { credentials in
                 // Do something with credentials e.g.: save them.
                 // Lock will not save these objects for you.
                 // Lock will dismiss itself automatically by default.
+                self.performSegue(withIdentifier: "segueOnSuccessfulLogin", sender: nil)
             }
             .present(from: self)
         
-        
-//        form +++ Section() { section in
-//            var customSection = HeaderFooterView<UIStackView>(.class)
-//            customSection.onSetupView = { view, section in
-//                view.backgroundColor = .white
-//            }
-//            section.header = customSection
-//            section.header?.height = {0}
-//            section.footer = customSection
-//            section.footer?.height = {60}
-//            section.reload()
-//            }
-//            <<< EmailRow("email") {
-//                $0.title = "Email"
-//                $0.placeholder = "your email goes here"
-//                $0.add(rule: RuleRequired())
-//                $0.add(rule: RuleEmail())
-//                }
-//                .cellUpdate { cell, row in
-//                    if !row.isValid { cell.titleLabel?.textColor = .red }
-//            }
-//            <<< PasswordRow("password") {
-//                $0.title = "Password"
-//                $0.placeholder = "password"
-//                $0.add(rule: RuleRequired())
-//                $0.add(rule: RuleMinLength(minLength: 8))
-//            }
-//            <<< ButtonRow() { row in
-//                row.title = "Login"
-//                row.cell.height = {90}
-//                row.validationOptions = .validatesOnChangeAfterBlurred
-//                row.disabled = Condition.function(["email", "password"], { form in
-//                    !(form.rowBy(tag: "email")?.isValid ?? false && form.rowBy(tag: "password")?.isValid ?? false)})
-//                }
-//                .onCellSelection {  cell, row in
-//                    let credentials = ["credentials": ["email" : self.form.values()["email"] as! String,
-//                                                      "password" : self.form.values()["password"] as! String]]
-//                    
-//                    Alamofire.request("https://nabroccoli.herokuapp.com/api/authenticate", method: .post, parameters: credentials, encoding: JSONEncoding.default)
-//                        .responseJSON { response in
-//                            print(response)
-//                            if let status = response.response?.statusCode {
-//                                switch(status) {
-//                                case 200:
-//                                    let alert = UIAlertController(title: "Authenticated!",
-//                                                                  message: "Hi broccoli! :)",
-//                                                                  preferredStyle: .alert)
-//                                    alert.addAction(UIAlertAction(title: "Yay", style: .default, handler: { (action) -> Void in self.performSegue(withIdentifier: "segueOnSuccessfulLogin", sender: nil) }))
-//                                    self.present(alert, animated: true, completion: nil)
-//                                    Keychain
-//                                case 404:
-//                                    let alert = UIAlertController(title: "Ups!",
-//                                                                  message: "Invalid credentials...",
-//                                                                  preferredStyle: .alert)
-//                                    alert.addAction(UIAlertAction(title: "Oke", style: .default, handler: { (action) -> Void in }))
-//                                    self.present(alert, animated: true, completion: nil)
-//                                default:
-//                                    print("error with response status: \(status)")
-//                                }
-//                            }
-//                        }
-//        }
     }
     
-    override func loadView() {
-        super.loadView()
-    }
 
     
     
