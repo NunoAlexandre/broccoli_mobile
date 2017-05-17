9502bf2b5e0395f09dda453d347104761f94b40d
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2017-05-17 20:39:42 +0200

    Styling the Lock

---
+++ ios/broccoli/BroccoliColors.swift
@@ -0,0 +1,33 @@
+//
+//  BroccoliColors.swift
+//  broccoli
+//
+//  Created by Nuno on 17/05/2017.
+//  Copyright © 2017 nunoalexandre. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    open class func broccoliYellow() -> UIColor {
+        return UIColor(red: 255/255, green: 252/255, blue: 214/255, alpha: 1.0)
+    }
+    
+    open class func broccoliDarkYellow() -> UIColor {
+        return UIColor(red: 225/255, green: 220/255, blue: 181/255, alpha: 1.0)
+    }
+    
+    open class func broccoliGreen() -> UIColor {
+        return UIColor(red: 136/255, green: 178/255, blue: 70/255, alpha: 1.0)
+    }
+    
+    open class func broccoliGreenLighter() -> UIColor {
+        return UIColor(red: 198/255, green: 211/255, blue: 95/255, alpha: 1.0)
+    }
+    
+    open class func broccoliGreenDarker() -> UIColor {
+        return UIColor(red: 177/255, green: 193/255, blue: 83/255, alpha: 1.0)
+    }
+}
