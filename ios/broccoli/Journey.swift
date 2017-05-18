4c0a559568b513e252aa6a8ca63abd044c33d178
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2017-05-18 20:51:26 +0200

    Setting popup on tapping a day in the graph

---
+++ ios/broccoli/Journey.swift
@@ -0,0 +1,29 @@
+//
+//  Journey.swift
+//  broccoli
+//
+//  Created by Nuno on 18/05/2017.
+//  Copyright © 2017 nunoalexandre. All rights reserved.
+//
+
+import Foundation
+
+class Journey {
+    let steps : [Step]
+    
+    init(data: [[String:Any]] ) {
+        steps = data.flatMap {
+            Step(day: ($0["day"] as? String)!, level: ($0["level"] as? String)!, note: ($0["note"] as? String)!)
+        }
+    }
+    
+    func days() -> [String] {
+        return steps.map{$0.day}
+    }
+    
+    func levels() -> [String] {
+        return steps.map{$0.level}
+    }
+    
+    
+}
