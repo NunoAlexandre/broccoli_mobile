3644b664691a09021e5c015d327dd88ac575e2f3
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2017-05-22 17:09:26 +0200

    Moved the AddDayForm api requests to BroccoliAPI and minor cleaning.

--- ios/broccoli/DayFormVC.swift
+++ ios/broccoli/DayFormVC.swift
@@ -1,11 +1,3 @@
-//
-//  UserDayFormViewController.swift
-//  broccoli
-//
-//  Created by Nuno on 23/04/2017.
-//  Copyright © 2017 nunoalexandre. All rights reserved.
-//
-
 import UIKit
 import Eureka
 import Alamofire
@@ -16,11 +8,11 @@ class DayFormVC : FormViewController {
         super.viewDidLoad()
         
         form +++ Section("Your day")
-            <<< DateRow("day") { row in
-                row.title = "Day"
-                row.add(rule: RuleRequired())
-                row.maximumDate = Date()
-                row.value = Date()
+            <<< DateRow("day") {
+                $0.title = "Day"
+                $0.add(rule: RuleRequired())
+                $0.maximumDate = Date()
+                $0.value = Date()
             }
             <<< ActionSheetRow<String>("level") {
                 $0.title = "Level"
@@ -28,47 +20,32 @@ class DayFormVC : FormViewController {
                 $0.options = ["twenty_one", "eight", "five", "three", "two", "one"]
                 $0.add(rule: RuleRequired())
             }
-            <<< TextAreaRow("note") { row in
-                row.title = "Note"
-                row.add(rule: RuleMaxLength(maxLength: 400))
+            <<< TextAreaRow("note") {
+                $0.title = "Note"
+                $0.add(rule: RuleMaxLength(maxLength: 400))
             }
-            <<< ButtonRow() { row in
-                    row.title = "Save"
-                    row.cell.height = {90}
+            <<< ButtonRow() {
+                    $0.title = "Save"
+                    $0.cell.height = {90}
                 }
                 .onCellSelection {  cell, row in
-                    let styled = DateFormatter()
-                    styled.dateFormat = "yyyy-MM-dd"
-                    let userDay = ["user_day" : ["day" : styled.string(from: (self.form.values()["day"] as! Date)),
-                                                 "level" : self.form.values()["level"] as? String,
-                                                 "note" : self.form.values()["note"] as? String]]
-                    
-                    let headers = ["Authorization": " Bearer \(IdToken.peek())" ]
-                    
-                    Alamofire.request("https://nabroccoli.herokuapp.com/api/days", method: .post, parameters: userDay,
-                                      encoding: JSONEncoding.default, headers: headers)
-                        .responseJSON { response in
-                            if let status = response.response?.statusCode {
-                                switch(status) {
-                                case 201:
-                                    let alert = UIAlertController(title: "Saved!", message: "well done, broccoli :)",
-                                                            preferredStyle: .alert)
-                                    alert.addAction(UIAlertAction(title: "Yay", style: .default, handler: { (action) -> Void in }))
-                                    self.present(alert, animated: true, completion: nil)
-                                case 422:
-                                    let alert = UIAlertController(title: "Ups!",
-                                                                  message: "You have already registered the selected day.",
-                                                                  preferredStyle: .alert)
-                                    alert.addAction(UIAlertAction(title: "Oke", style: .default, handler: { (action) -> Void in }))
-                                    self.present(alert, animated: true, completion: nil)
-                                case 401:
-                                    self.performExplainedLogout()
-                                default:
-                                    print("error with response status: \(status)")
-                                }
-                            }
-                    }
+                        BroccoliAPI().saveDay(self.userDay(from: self.form),
+                                          onSuccess: {self.present(Feedback.dayAdded())},
+                                          onDuplicateDay: {self.present(Feedback.duplicatedDay())},
+                                          onUnauthorized: self.performExplainedLogout)
                 }
+    }
+    
+    private func asString(_ date : Date) -> String {
+        let styled = DateFormatter()
+        styled.dateFormat = "yyyy-MM-dd"
+        return styled.string(from: date)
+    }
+    
+    func userDay(from form: Form) -> Parameters {
+        return ["user_day" : ["day" : self.asString(self.form.values()["day"] as! Date),
+                          "level" : self.form.values()["level"] as? String,
+                          "note" : self.form.values()["note"] as? String]]
     }
     
 }
