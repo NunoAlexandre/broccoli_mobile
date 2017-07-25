db0c163e81680e872735b2819ff905060f10adf3
NunoAlexandre <alexandre.nunomiguel@gmail.com>
2017-07-25 08:43:28 +0200

    Create JourneyStats

--- ios/broccoli/JourneyStats.swift
+++ ios/broccoli/JourneyStats.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import SigmaSwiftStatistics
+
+
+class JourneyStats {
+    private let journey : Journey
+    
+    init(_ journey: Journey ) {
+        self.journey = journey
+    }
+    
+    func average() -> Int {
+        return calculate { Sigma.average($0)! }
+    }
+    
+    func median() -> Int {
+        return calculate { Sigma.median($0)! }
+    }
+    
+    private func calculate(_ function : ([Double]) -> Double) -> Int {
+        return Int(function(journey.daysList.map{Double($0.level)}))
+    }
+}
